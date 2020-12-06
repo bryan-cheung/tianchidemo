b7a6031600fed9cf584206dc1d901d4e1bed2de8
bryan-cheung <bryan.l.cheung@gmail.com>
2020-12-06 22:16:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## 整体流程交互
+本demo主要是用于演示数据交互过程，性能很一般，仅供各选手参考
+![enter image description here](https://tianchi-public.oss-cn-hangzhou.aliyuncs.com/public/files/forum/158937741003137571589377409737.png)
+```shell script
+docker build -f /Users/lizhang/Documents/tianchi/demo/target/DockerFile -t registry.cn-shanghai.aliyuncs.com/bryan_cheung/bryan:v12 .
+```
+  
+```shell script
+docker push registry.cn-shanghai.aliyuncs.com/bryan_cheung/bryan:v12  
+```  
+ 
+ 
+ 
---
+++ hostFile.py
@@ -0,0 +1,37 @@
+try:
+    import http.server as BaseHTTPServer 
+except:
+    import BaseHTTPServer
+
+import os
+import shutil
+import sys
+
+FILEPATH = "/Users/lizhang/Documents/tianchi/large/trace1.data"
+
+class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        with open(FILEPATH, 'rb') as f:
+            self.send_response(200)
+            self.send_header("Content-Type", 'applicaton/octet-stream')
+            self.send_header("Content-Disposition", 'attachment; filename="{}"'.format(os.path.basename(FILEPATH)))
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs.st_size))
+            self.end_headers()
+            shutil.copyfileobj(f, self.wfile)
+
+def test(HandlerClass=SimpleHTTPRequestHandler,
+        ServerClass=BaseHTTPServer.HTTPServer,
+        protocol="HTTP/1.1"):
+    port = 8301
+    server_address = ('', port)
+
+    HandlerClass.protocol_version = protocol
+    httpd = BaseHTTPServer.HTTPServer(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    print("Serving HTTP on {0[0]} port {0[1]} ... {1}".format(sa, FILEPATH))
+    httpd.serve_forever()
+
+test()
+
